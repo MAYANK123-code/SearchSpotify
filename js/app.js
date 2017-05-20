c897bf0b7147d1f6c91813cd6d6ef0b47229f48e
Mark Ledbetter <markleds@gmail.com>
2017-05-20 14:12:22 -0400

    additional CSS styling and edits to templates

--- js/app.js
+++ js/app.js
@@ -8,23 +8,36 @@ const API_URL_BASE = 'https://api.spotify.com/v1/search?q=';
 // API_URL = API_URL_BASE
 $(document).ready(function() {
   const $searchForm = $('#seach-form');
-  const $resultsLocation = $('#results-location');
+  const $resultsLocation = $("#results-location");
   var albumSearch = "Hornsby";
   var category = "album";
-  console.log();
-  const runSearch = function() {
-    $.ajax({
-        type: 'GET',
-        url: `${API_URL_BASE}${albumSearch}&type=${category}`,
-      })
-      .then(function(results) {
-        console.log(results);
+  // const showSpotify = (music) => {
+  //   // execute the dynamic version with our data
+  //   // our template is expecting a key called 'music'
+  //   // we'll use an ES6 shorthand:
+  //   // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+  //   const compiledTemplate = Handlebars.templates.spotifyList({
+  //     music
+  //   });
+  //
+  //
+  //   // swap out the destination placeholder with our new HTML
+  //   $resultsLocation.html(compiledTemplate);
+  // };
 
-        // form.reset()
-      })
-      // .then(printResults)
-      .catch(e => console.error(e));
-  };
+  // const runSearch = function() {
+  //   $.ajax({
+  //       type: 'GET',
+  //       url: `${API_URL_BASE}${albumSearch}&type=${category}`,
+  //     })
+  //     .then(function(showSpotify) {
+  //       // console.log(results);
+  //
+  //       // form.reset()
+  //     })
+  //     // .then(printResults)
+  //     .catch(e => console.error(e));
+  // };
 
 
 
@@ -33,6 +46,7 @@ $(document).ready(function() {
     event.preventDefault();
     // stop the event from porpagating
     event.stopPropagation();
+
     const form = event.currentTarget;
     const inputs = form.elements;
     console.log(inputs);
@@ -41,26 +55,108 @@ $(document).ready(function() {
       query: inputs.searchParam.value,
     }
     console.log(formData);
-    var category = $("#search-category").val();
-    // let category = form.inputs.searchCategory;
-    console.log(category);
-    // category = "album";
-    let searchInput = $(".search-input").val();
-    console.log(searchInput);
-    const runSearch = function() {
+
+    // const showSpotify = function(people) {
+    //   $("#results-location").html("Hello There");
+    //   // execute the dynamic version with our data
+    //   // our template is expecting a key called 'music'
+    //   // we'll use an ES6 shorthand:
+    //   // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+    //   const compiledTemplate = Handlebars.templates.spotifyList({
+    //     people
+    //   });
+    //
+    //
+    //   // swap out the destination placeholder with our new HTML
+    //   $("#results-location").html(compiledTemplate);
+    // };
+
+    const runSearchArtist = function() {
       $.ajax({
           type: 'GET',
           url: `https://api.spotify.com/v1/search?q=${formData.query}&type=${formData.category}`,
         })
-        .then(function(results) {
-          console.log(results);
+        .then(function(artists) {
+          console.log(artists);
+          // showSpotify(results);
+          $("#results-location").html("Hello There");
+          // execute the dynamic version with our data
+          // our template is expecting a key called 'artists'
+          // we'll use an ES6 shorthand:
+          // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+          const compiledTemplate = Handlebars.templates.spotifyListartist({
+            artists
+          });
 
-          // form.reset()
+
+          // swap out the destination placeholder with our new HTML
+          $("#results-location").html(compiledTemplate);
+
+          form.reset()
         })
         // .then(printResults)
         .catch(e => console.error(e));
     };
-    runSearch();
+    const runSearchAlbum = function() {
+      $.ajax({
+          type: 'GET',
+          url: `https://api.spotify.com/v1/search?q=${formData.query}&type=${formData.category}`,
+        })
+        .then(function(artists) {
+          console.log(artists);
+          // showSpotify(results);
+          $("#results-location").html("Hello There");
+          // execute the dynamic version with our data
+          // our template is expecting a key called 'artists'
+          // we'll use an ES6 shorthand:
+          // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+          const compiledTemplate = Handlebars.templates.spotifyListalbum({
+            artists
+          });
+
+
+          // swap out the destination placeholder with our new HTML
+          $("#results-location").html(compiledTemplate);
+
+          form.reset()
+        })
+        // .then(printResults)
+        .catch(e => console.error(e));
+    };
+    const runSearchSong = function() {
+      $.ajax({
+          type: 'GET',
+          url: `https://api.spotify.com/v1/search?q=${formData.query}&type=${formData.category}`,
+        })
+        .then(function(artists) {
+          console.log(artists);
+          // showSpotify(results);
+          $("#results-location").html("Hello There");
+          // execute the dynamic version with our data
+          // our template is expecting a key called 'artists'
+          // we'll use an ES6 shorthand:
+          // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
+          const compiledTemplate = Handlebars.templates.spotifyListsong({
+            artists
+          });
+
+
+          // swap out the destination placeholder with our new HTML
+          $("#results-location").html(compiledTemplate);
+
+          form.reset()
+        })
+        // .then(printResults)
+        .catch(e => console.error(e));
+    };
+    if (formData.category === "artist") {
+      runSearchArtist();
+    } else if (formData.category === "album") {
+      runSearchAlbum();
+    } else if (formData.category === "track") {
+      runSearchSong();
+      category
+    }
 
 
   });
