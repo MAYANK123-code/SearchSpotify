4e50528060029e99a4eaf976348c7b69583aef72
Mark Ledbetter <markleds@gmail.com>
2017-05-22 01:01:39 -0400

    update to template

--- js/templates/templatesCompiled.js
+++ js/templates/templatesCompiled.js
@@ -5,22 +5,26 @@ templates['spotifyListalbum'] = template({"1":function(container,depth0,helpers,
 
   return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "");
 },"2":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression, alias5=container.lambda;
 
-  return "<div class=\"result\">\n  <div class=\"result-image\"><img class=\"result-image-style\" src=\""
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.url : stack1), depth0))
+  return "<div class=\"result\">\n  <div class=\"result-image\" data-album-title=\""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + " "
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.name : stack1), depth0))
+    + "\"><img class=\"result-image-style\" src=\""
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.url : stack1), depth0))
     + "\" /></div>\n  <div class=\"result-title\">"
-    + alias2(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</div>\n  <div data-artist-name=\""
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.name : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.name : stack1), depth0))
     + "\" class=\"result-title-2\"><a>"
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.name : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.name : stack1), depth0))
     + "</a></div>\n  <div data-artist-name=\""
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["1"] : stack1)) != null ? stack1.name : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["1"] : stack1)) != null ? stack1.name : stack1), depth0))
     + "\" class=\"result-title-2\"><a>"
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["1"] : stack1)) != null ? stack1.name : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.artists : depth0)) != null ? stack1["1"] : stack1)) != null ? stack1.name : stack1), depth0))
     + "</a></div>\n  <div class=\"result-on-spotify\">Learn more on</br>\n    <a href=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.external_urls : depth0)) != null ? stack1.spotify : stack1), depth0))
+    + alias4(alias5(((stack1 = (depth0 != null ? depth0.external_urls : depth0)) != null ? stack1.spotify : stack1), depth0))
     + "\" target=\"_blank\">\n      <img src=\"spotify-1.png\" height=\"25px\" width=\"76px\" /></a></div>\n</div>\n";
 },"4":function(container,depth0,helpers,partials,data) {
     return "<p>sorry, no super heros here</p>\n";
@@ -34,16 +38,18 @@ templates['spotifyListartist'] = template({"1":function(container,depth0,helpers
 
   return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "");
 },"2":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), alias4=helpers.helperMissing, alias5="function";
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression, alias5=container.lambda;
 
-  return "<div class=\"result\">\n  <div class=\"result-image\"><img class=\"result-image-style\" src=\""
-    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.url : stack1), depth0))
+  return "<div class=\"result\">\n  <div class=\"result-image\" data-album-title=\""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\"><img class=\"result-image-style\" src=\""
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.url : stack1), depth0))
     + "\" /></div>\n  <div class=\"result-title\">"
-    + alias2(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"name","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</div>\n  <div class=\"view-albums\"><button data-artist-name=\""
-    + alias2(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"name","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "\" class=\"album-buttom\" type=\"button\" name=\"albums\"><a>Albums</a></button>\n\n  </div>\n  <div class=\"result-on-spotify\">Learn more on</br>\n    <a href=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.external_urls : depth0)) != null ? stack1.spotify : stack1), depth0))
+    + alias4(alias5(((stack1 = (depth0 != null ? depth0.external_urls : depth0)) != null ? stack1.spotify : stack1), depth0))
     + "\" target=\"_blank\">\n    <img src=\"spotify-1.png\" height=\"25px\" width=\"76px\" /></a></div>\n</div>\n";
 },"4":function(container,depth0,helpers,partials,data) {
     return "<p>sorry, no super heros here</p>\n";
